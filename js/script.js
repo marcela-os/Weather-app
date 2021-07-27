5e0b289414b155a1fe11b281b721d7c354c994bd
Marcelina <m.oscislowska@gmail.com>
2021-07-27 16:15:13 +0200

    Added function getLocation to .js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 
 function startApp(){
     console.log("window.onload is working");
+    getLocation();
 }
 window.onload = startApp;
+
+function getLocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition((position) => {
+            lat = position.coords.latitude;
+            long = position.coords.longitude;
+
+            console.log("lat: " + lat, "long: " + long);
+        });
+    } 
+}
