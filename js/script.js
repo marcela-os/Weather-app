b202ac33d697503ee82d18149adf96781bd220cc
Marcelina <m.oscislowska@gmail.com>
2021-07-27 16:29:31 +0200

    Added function getWeaherData to .js

--- js/script.js
+++ js/script.js
@@ -8,6 +8,8 @@ window.onload = startApp;
 
 let lat;
 let long;
+const apiKey = config.API_KEY;
+
 
 function getLocation(){
     if(navigator.geolocation){
@@ -16,6 +18,13 @@ function getLocation(){
             long = position.coords.longitude;
 
             console.log("lat: " + lat, "long: " + long);
+
+            getWeatherData();
         });
     } 
+}
+
+function getWeatherData() {
+    let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=metric&appid=${apiKey}`;
+    console.log(url);
 }
